67cde4d2b858a09604259bb169d17bf7b9e5dfda
VISHNU M K <futuralabs.mern@gmail.com>
2025-02-13 10:53:27 +0530

    stream

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -19,38 +19,68 @@
 // fs, http, os, path, url, dns -> Built-in Modules
 // nodemon, bcrypt, jsonwebtoken -> Third-party Modules
 
-const os = require("os")
+const os = require("os") // ES5
+// import os from "os" // ES6
 
-console.log(os.platform());
-console.log(os.version());
-console.log(os.homedir());
-console.log(os.tmpdir());
-console.log(os.hostname());
-console.log(os.uptime());
-console.log(os.cpus());
-console.log(os.networkInterfaces());
+// console.log(os.platform());
+// console.log(os.version());
+// console.log(os.homedir());
+// console.log(os.tmpdir());
+// console.log(os.hostname());
+// console.log(os.uptime());
+// console.log(os.cpus());
+// console.log(os.networkInterfaces());
 
 
-const fs = require("fs") // file System
+const fs = require("fs") // file System - ES5
+// import fs from "fs" // ES6
 
-fs.writeFile("index.txt", "Hello World", () => {
-    console.log("File Created") 
+// fs.writeFile("index.txt", "Hello World", () => {
+//     console.log("File Created") 
+// })
+
+// fs.readFile("index.txt", "utf-8", (error, content) => {
+//     if (error) {
+//         return console.log(error.message);
+//     }
+//     return console.log("Content: ",content);
+// })
+ 
+// fs.appendFile("index.txt", " Sample text", () => {
+//     console.log("File Updated")    
+// })
+
+// fs.unlink("index.txt", (error) => {
+//     if (error) {
+//         return console.log(error.message)
+//     }
+//     return console.log("File Deleted")
+// })
+
+fs.rename("index.txt", "renamedIndex.txt", () => {
+    // console.log("File Renamed");
 })
 
-fs.readFile("index.txt", "utf-8", (error, content) => {
-    if (error) {
-        return console.log(error.message);
-    }
-    return console.log("Content: ",content);
+// streams => chunks => readStream, writeStream
+
+const readStream = fs.createReadStream("sample.txt", { encoding: "utf-8" }) // Buffer
+const writeStream = fs.createWriteStream("result.txt", { encoding: "utf-8" })
+// data, end, error
+
+// readStream.on("data", (chunk) => {
+//     writeStream.write(`CHUNK-START: ${chunk}`)
+// })
+
+readStream.pipe(writeStream)
+
+readStream.on("error", (error) => {
+    console.log(error.message);
 })
  
-fs.appendFile("index.txt", " Sample text", () => {
-    console.log("File Updated")    
+readStream.on("end", () => {
+    console.log("File Read Completed");
 })
 
-fs.unlink("index.txt", (error) => {
-    if (error) {
-        return console.log(error.message)
-    }
-    return console.log("File Deleted") 
-})
+ 
+
+
