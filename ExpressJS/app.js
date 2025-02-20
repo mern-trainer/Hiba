a5014a2fd3ad0beffc1ec585733a94081e814718
VISHNU M K <futuralabs.mern@gmail.com>
2025-02-20 10:59:22 +0530

    todo list with express js

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,8 +1,7 @@
 const express = require("express")
+const { v4: uuidv4 } = require("uuid")
 
 const app = express()
-
-// GET, POST, PUT, DELETE, PATCH
 
 app.use(express.json())
 
@@ -17,6 +16,53 @@ app.post("/", (request, response) => {
     if (!title) {
         return response.status(400).send({ message: "Please provide a title" })
     }
+    const index = todoList.findIndex(todo => todo.title.toLowerCase() === title.toLowerCase())
+    if (index > -1) {
+        return response.status(409).send({ message: "Task already exists" })
+    }
+    const dateTime = new Date().toISOString()
+    const taskObj = {
+        id: uuidv4(),
+        title,
+        completed: false,
+        createdAt: dateTime,
+        updatedAt: dateTime
+    }
+    todoList.unshift(taskObj)
+    return response.status(201).send({ created: taskObj, message: "Task created successfully" })
+})
+
+app.delete("/:id?", (request, response) => {
+    const { id } = request.params
+    if (!id) {
+        return response.status(400).send({ message: "Please provide an id" })
+    }
+    const deleted = todoList.find(todo => todo.id === id)
+    if (!deleted) {
+        return response.status(404).send({ message: "Task not found" })
+    }
+    todoList = todoList.filter(todo => todo.id !== id)
+    return response.status(200).send({deleted, message: "Task deleted successfully"})
+})
+
+app.patch("/:id?", (request, response) => {
+    const { id } = request.params
+    const { completed, title } = request.body
+    console.log(completed, title);
+    if (!id) {
+        return response.status(400).send({ message: "Please provide an id" })
+    }
+    const index = todoList.findIndex(todo => todo.id === id)
+    if (index == -1) {
+        return response.status(404).send({ message: "Task not found" })
+    }
+    if (completed != undefined && (completed == true || completed == false)) {
+        todoList[index].completed = completed
+    }
+    if (title != undefined) {
+        todoList[index].title = title
+    }
+    return response.status(200).send({updated: todoList[index], message: "Task updated successfully"})
 })
 
 app.listen(8080, () => {
